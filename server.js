02d0174666be58afdd9a3a51fb5fbac835099d7d
oluwadaprof <adeekotobiloba8@gmail.com>
2022-08-19 12:30:03 +0100

    project setup

---
+++ server.js
@@ -0,0 +1,14 @@
+const logEvents = require('./logEvents');
+const EventEmitter = require('events');
+class Emitter extends EventEmitter {};
+
+//intialize object
+const myEmitter = new Emitter();
+
+//add listener for the log event
+myEmitter.on('log', (msg)=> logEvents(msg))
+
+setTimeout(()=>{
+    //Emit event
+    myEmitter.emit('log', 'Log event emitted!')
+}, 2000 ) 
